44895386238216635654000766cfe03be9383566
Felipe <fas.felipesantos@gmail.com>
2023-02-27 07:26:43 -0300

    .

---
+++ src/components/Menu.js
@@ -0,0 +1,44 @@
+import React from "react";
+import '../App.css';
+
+const Menu = ({ setLineColor, setLineWidth, setLineOpacity }) => {
+
+  return (
+    <div className="Menu">
+      <label>Brush Color</label>
+      <input
+        type='color'
+        onChange={(e) => {
+          setLineColor(e.target.value);
+        }}
+      />
+      <label>Brush Width</label>
+      <input
+        type='range'
+        min="3"
+        max="20"
+        onChange={(e) => {
+          setLineWidth(e.target.value);
+        }}
+      />
+      <label>Brush Opacity</label>
+      <input
+        type='range'
+        min="1"
+        max="100"
+        onChange={(e) => {
+          setLineOpacity(e.target.value / 100);
+        }}
+      />
+      {/* TODO
+
+        ERASER
+        CLEAN THE BOARD
+        PUT/HIDE GRID
+        DRAW GEOMETRIC SYMBOLS
+      */}
+    </div>
+  )
+}
+
+export default Menu;
