5e8b80a7f5f16cefc01da03a9892536411c9c64f
Felipe <fas.felipesantos@gmail.com>
2023-03-05 15:06:40 -0300

    .

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { useEffect, useRef, useState } from 'react';
 import Menu from './components/Menu';
 import './App.css';
 
+const DEFAULT_BACKGROUND_COLOR = 'white';
+
 function App() {
   const canvasRef = useRef(null);
   const ctxRef = useRef(null);
   const canvasBackgroundRef = useRef(null);
   const ctxBackgroundRef = useRef(null);
-
+  const canvasGridRef = useRef(null);
+  const ctxGridRef = useRef(null);
 
   const [isDrawing, setIsDrawing] = useState(false);
   const [lineWidth, setLineWidth] = useState(5);
   const [lineColor, setLineColor] = useState('black');
-  const [backgroundColor, setBackgroundColor] = useState('white');
+  const [backgroundColor, setBackgroundColor] = useState(DEFAULT_BACKGROUND_COLOR);
   const [lineOpacity, setLineOpacity] = useState(0.1);
+  const [tool, setTool] = useState('pencil');
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -40,7 +44,6 @@ function App() {
   }, [backgroundColor]);
 
   const startDrawing = (e) => {
-    console.log('oxe')
     ctxRef.current.beginPath();
     ctxRef.current.moveTo(
       e.nativeEvent.offsetX,
@@ -58,11 +61,29 @@ function App() {
     if (!isDrawing) {
       return;
     }
-    ctxRef.current.lineTo(
-      e.nativeEvent.offsetX,
-      e.nativeEvent.offsetY
-    );
-    ctxRef.current.stroke();
+    else {
+      if (tool === 'pencil') {
+        ctxRef.current.lineTo(
+          e.nativeEvent.offsetX,
+          e.nativeEvent.offsetY
+        );
+        ctxRef.current.stroke();
+      }
+      else if (tool === 'eraser') {
+        ctxRef.current.clearRect(
+          e.nativeEvent.offsetX,
+          e.nativeEvent.offsetY,
+          lineWidth * 2,
+          lineWidth * 2
+        );
+      }
+    }
+
+  };
+
+  const reset = () => {
+    const canvas = canvasRef.current;
+    ctxRef.current.clearRect(0, 0, canvas.width, canvas.height);
   }
 
 
@@ -77,9 +98,17 @@ function App() {
           setLineWidth={setLineWidth}
           setLineOpacity={setLineOpacity}
           setBackgroundColor={setBackgroundColor}
+          reset={reset}
+          setTool={setTool}
+          tool={tool}
         />
         <canvas
           ref={canvasBackgroundRef}
+          width={'1280px'}
+          height={'720px'}
+        />
+        <canvas
+          ref={canvasGridRef}
           width={'1280px'}
           height={'720px'}
         />
@@ -92,6 +121,7 @@ function App() {
           height={'720px'}
         />
       </div>
+      <h2>by Sotnas</h2>
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,49 @@
 import React from "react";
 import '../App.css';
 
-const Menu = ({ setLineColor, setLineWidth, setLineOpacity, setBackgroundColor }) => {
+const Menu = ({
+  setLineColor
+  , setLineWidth
+  , setLineOpacity
+  , setBackgroundColor
+  , reset
+  , tool
+  , setTool
+}) => {
 
   return (
     <div className="Menu">
+      <label class="tooltip">Grid</label>
+      <div class="btn-plus">
+        <ul>
+          <li><a href="#about">No Grid</a></li>
+          <li><a href="#blog">2x2</a></li>
+          <li><a href="#projects">3x3</a></li>
+          <li><a href="#contact">4x4</a></li>
+        </ul>
+      </div>
+      <input
+        type="radio"
+        id="pencil"
+        name="drawing"
+        value="pencil"
+        checked={tool === 'pencil' ? true : false}
+        onClick={(e) => {
+          setTool(e.target.value);
+        }}
+      />
+      <label for="pencil">Pencil</label>
+      <input
+        type="radio"
+        id="eraser"
+        name="drawing"
+        value="eraser"
+        checked={tool === 'eraser' ? true : false}
+        onClick={(e) => {
+          setTool(e.target.value);
+        }}
+      />
+      <label for="eraser">Eraser</label>
       <label>Brush Color</label>
       <input
         type='color'
@@ -37,10 +76,13 @@ const Menu = ({ setLineColor, setLineWidth, setLineOpacity, setBackgroundColor }
           setBackgroundColor(e.target.value);
         }}
       />
+      <input
+        type='button'
+        value='Reset'
+        onClick={reset}
+      />
       {/* TODO
 
-        ERASER
-        CLEAN THE BOARD
         PUT/HIDE GRID
         DRAW GEOMETRIC SYMBOLS (CIRCLE, SQUARE, TRIANGLE)
         WRITE
